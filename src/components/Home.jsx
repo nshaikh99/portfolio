c3ef6ed97623f18995dccfe5c40d9a5bcdc27acf
Nabeel Shaikh <nabeelshaikh@g.ucla.edu>
2024-05-20 19:58:47 -0700

    Updated graduation status

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
             <FadeIn>
               <p className='text-pink-600'>Hi, my name is</p>
               <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Nabeel Shaikh</h1>
-              <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a computer science student at UCLA.</h2>
+              <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a recent CS graduate from UCLA.</h2>
               <p className='text-[#8892b0] py-4 max-w-[700px]'>Welcome to my personal website. This is where I like to showcase the latest projects that I've been working on.</p>
               <div className='flex flex-col sm:flex-row gap-4'>
                   <a href={Resume} target='_blank'><button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>Download my resume<span><HiDocumentDownload className='ml-3' /></span></button></a>
